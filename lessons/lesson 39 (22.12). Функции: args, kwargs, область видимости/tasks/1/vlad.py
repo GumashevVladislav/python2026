0ec928795a518a2f6bf872fe7ecaebe35390c3cc
GumashevVladislav <zelofan3@gmail.com>
2025-12-22 19:55:37 +0500

    Create vlad.py

---
+++ lessons/lesson 39 (22.12). Функции: args, kwargs, область видимости/tasks/1/vlad.py
@@ -0,0 +1,6 @@
+print(mean_absdeviation(3, 5, 7, 9))
+      if not scores:
+         raise ValueError("нужно передать хот бы один балл")
+
+         existing_scores = [10, 8, 6]
+         print(mean_abs_deviation(*existing_scores))
